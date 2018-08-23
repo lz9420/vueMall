4fdbfe6f0f61b8d759622a069f2b001ac45b3000
lz9420 <693020101@qq.com>
2018-08-23 22:18:11 +0800

    商品列表渲染

--- src/App.vue
+++ src/App.vue
@@ -37,9 +37,10 @@
                 <div id="menu2" class="nav-box menuhd">
                     <ul>
                         <li class="index">
-                            <a href="#" class="">
+                            <router-link to="/index">
                                 <span class="out" style="top: 0px;">首页</span>
-                            </a>
+                            </router-link>
+                            
                         </li>
                         <li class="news">
                             <a href="#" class="">
@@ -79,6 +80,8 @@
             </div>
         </div>
     </div>
+     <!-- 出口 渲染到什么位置 -->
+        <router-view></router-view>
     <div class="footer">
                 <div class="section">
                     <div class="foot-nav">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
+// 导入路由
+import VueRouter from 'vue-router'
 import App from './App.vue'
+// 导入首页的组件
+import Index from './components/01.index.vue';
 
+Vue.use(VueRouter);
+
+// 定义路由规则
+let routes = [
+  {
+    // 默认进来就是首页
+    path:'/',
+    component:Index,
+  },
+  {
+    path:'/index',
+    component:Index,
+  },
+]
+// 实例化路由对象
+let router = new VueRouter({
+  routes
+})
+
+// 挂载到vue实例上
 Vue.config.productionTip = false
 
+
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // 路由对象
+  router
 }).$mount('#app')
